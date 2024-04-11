de4c3ed8d4cea2c5e3dd330cd409d214f35deaab
BetaPeche <lolopellegrino@gmail.com>
2024-04-11 19:12:54 +0200

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { NavLink, Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <header>
+            <Link to="/">Loïc Pellegrino</Link>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={(nav) =>
+                                nav.isActive ? 'active-link' : ''
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
