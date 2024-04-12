4da266bbea617276bf388300688fdccef86b06c1
BetaPeche <lolopellegrino@gmail.com>
2024-04-12 22:23:05 +0200

    Init component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,24 @@
+import useThemeStore from '../themeStore'
+
 const Banner = () => {
-    return <div></div>
+    const { theme } = useThemeStore()
+
+    return (
+        <div className="banner">
+            <img
+                src={
+                    theme === 'light'
+                        ? './img/ImagePortfolio.webp'
+                        : './img/ImagePortfolioDark.webp'
+                }
+                alt=""
+            />
+            <div className="banner-content">
+                <h1>Bonjour !</h1>
+                <p>Test</p>
+            </div>
+        </div>
+    )
 }
 
 export default Banner
