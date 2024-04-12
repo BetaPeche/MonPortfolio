87d44b9be42b705bbda76701b0563589e320d3c0
BetaPeche <lolopellegrino@gmail.com>
2024-04-12 13:59:37 +0200

    Add toggle theme mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
+    const [darkMode, setDarkMode] = useState(true)
+    useEffect(() => {
+        if (localStorage.getItem('theme') === 'light') {
+            setDarkMode(false)
+            isLight()
+        } else {
+            setDarkMode(true)
+            isDark()
+        }
+    }, [])
+
+    const isDark = () => {
+        document.body.classList.remove('light')
+        document.body.classList.add('dark')
+        localStorage.setItem('theme', 'dark')
+    }
+
+    const isLight = () => {
+        document.body.classList.remove('dark')
+        document.body.classList.add('light')
+        localStorage.setItem('theme', 'light')
+    }
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode)
+        if (darkMode) {
+            isLight()
+        } else {
+            isDark()
+        }
+    }
+
     return (
         <header>
             <Link to="/">Loïc Pellegrino</Link>
@@ -20,6 +53,10 @@ const Header = () => {
                         <NavLink to="/about">About</NavLink>
                     </li>
                 </ul>
+                <button
+                    className={darkMode ? 'dark' : 'light'}
+                    onClick={toggleDarkMode}
+                ></button>
             </nav>
         </header>
     )
