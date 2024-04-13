ce734d5a488fc6967128d428f027e2e071bbfdba
BetaPeche <lolopellegrino@gmail.com>
2024-04-13 12:51:15 +0200

    Add 'compétences' bloc

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,33 @@
 import Banner from '../components/Banner'
+import DataSkills from '../assets/data/dataSkills.json'
+import useThemeStore from '../themeStore'
 
 const Home = () => {
-    return <Banner />
+    const { theme } = useThemeStore()
+    return (
+        <main>
+            <Banner />
+            <h2>Compétences</h2>
+            <section className="skills">
+                {DataSkills.map((skill) => (
+                    <div className="skill" key={skill.name}>
+                        <div className="skill-image">
+                            <img
+                                src={
+                                    theme === 'dark' && skill.darkimage
+                                        ? skill.darkimage
+                                        : skill.image
+                                }
+                                alt={`logo ${skill.name}`}
+                            />
+                        </div>
+                        <p>{skill.name}</p>
+                    </div>
+                ))}
+            </section>
+            <h2>Projets</h2>
+        </main>
+    )
 }
 
 export default Home
