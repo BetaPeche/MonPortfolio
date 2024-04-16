b8dc24a795d397ee544eac01cf709e79c938d89d
BetaPeche <lolopellegrino@gmail.com>
2024-04-16 15:56:20 +0200

    Add content on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,12 @@
 const Footer = () => {
-    return <footer>Footer</footer>
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>Make with &#x1FA77; by me</p>
+                <div className="footer-links">Test</div>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
