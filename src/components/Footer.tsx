a51e07d3f074e7598b62afffdcdfbf83e6f86de1
BetaPeche <lolopellegrino@gmail.com>
2024-04-16 16:43:54 +0200

    Add links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,26 @@
+import { Link } from 'react-router-dom'
+
 const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
                 <p>Make with &#x1FA77; by me</p>
-                <div className="footer-links">Test</div>
+                <div className="footer-links">
+                    <Link
+                        to="https://github.com/BetaPeche"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="./img/githubdark.webp" alt="logo github" />
+                    </Link>
+                    <Link
+                        to="https://www.linkedin.com/in/lo%C3%AFc-pellegrino-927295295/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="./img/linkedin.webp" alt="logo linkedin" />
+                    </Link>
+                </div>
             </div>
         </footer>
     )
