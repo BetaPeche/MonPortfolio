456005e8e8fb410b5e5786c41296b755d5d29b46
BetaPeche <lolopellegrino@gmail.com>
2024-04-17 15:16:48 +0200

    Add structure of component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,13 @@
-const Card = () => {
-    return <div></div>
+import { Link } from 'react-router-dom'
+
+const Card = ({ project }) => {
+    return (
+        <Link to="/" className="test">
+            <h3>Kasa</h3>
+            <p>UX • UI Design</p>
+            <img src="./img/mac.png" alt="" />
+        </Link>
+    )
 }
 
 export default Card
