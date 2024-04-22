6353120618b97f9b4d9e7ccea1cd37534d1413c2
BetaPeche <lolopellegrino@gmail.com>
2024-04-22 13:47:32 +0200

    Add "about-me" section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,6 +21,28 @@ const Home = () => {
                 ))}
             </section>
             <h2>Qui suis-je ?</h2>
+            <section className="about-me">
+                <img src="./img/moua.png" alt="" />
+                <p>
+                    Bonjour, je suis un développeur web passionné basé dans un
+                    charmant village de l'Aveyron. À 33 ans, père de deux
+                    merveilleux enfants, je suis un amoureux de la nature et un
+                    curieux invétéré. <br />
+                    <br />
+                    Après avoir passé 13 ans dans l'industrie agroalimentaire,
+                    j'ai entrepris un projet entrepreneurial ambitieux de rachat
+                    d'usine. Bien que ce projet n'ait pas abouti comme prévu, il
+                    m'a inspiré à suivre ma véritable passion : l'informatique
+                    et le développement web.
+                    <br />
+                    <br /> En transition vers une nouvelle carrière dans le
+                    développement web, chaque projet renforce mes compétences et
+                    ma passion pour ce domaine fascinant.
+                    <br />
+                    <br /> Merci de prendre le temps de découvrir mon parcours
+                    et mes projets !
+                </p>
+            </section>
         </main>
     )
 }
