19424ef3c7be7b9dd43019bff483c56a3996e35f
BetaPeche <lolopellegrino@gmail.com>
2024-04-25 12:41:50 +0200

    Remove useffect and and class in 'root'

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { useEffect } from 'react'
 import useThemeStore from './themeStore'
 import Projet from './pages/Projet'
 
 function App() {
     const { theme } = useThemeStore()
+    const root = document.getElementById('root')
 
-    useEffect(() => {
-        if (theme === 'dark') {
-            document.body.className = 'dark'
-        } else {
-            document.body.className = 'light'
-        }
-    }, [theme])
+    if (!root) {
+        return null
+    }
+
+    if (theme === 'dark') {
+        root.className = 'dark'
+    } else {
+        root.className = 'light'
+    }
 
     return (
         <BrowserRouter>
@@ -23,7 +25,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/projet/:id" element={<Projet />} />
-                {/* <Route path="*" element={<Home />} /> */}
+                <Route path="*" element={<Home />} />
             </Routes>
             <Footer />
         </BrowserRouter>
