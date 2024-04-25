7919db550df2cb0a0f8a24aff75babeaf35b0447
BetaPeche <lolopellegrino@gmail.com>
2024-04-25 12:41:23 +0200

    Add type, show data of project

--- src/pages/Projet.tsx
+++ src/pages/Projet.tsx
@@ -1,22 +1,50 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import dataProjects from '../assets/data/dataProjects.json'
 
 const Projet = () => {
-    const { id } = useParams()
-    console.log(id)
+    const { id } = useParams<{ id?: string }>()
+    const [project, setProject] = useState<{
+        name: string
+        keywords: string
+        description: string
+        image: string
+        banner: string
+        link: string
+    } | null>(null)
     const navigate = useNavigate()
-    // const [hosting, setHosting] = useState()
 
     useEffect(() => {
-        const data = dataProjects.find((array) => array.name == id)
-        if (!data) {
+        if (!id) {
             navigate('/')
+            return
+        }
+
+        const modifiedId = id.replace('-', ' ')
+
+        const data = dataProjects.find((array) => array.name == modifiedId)
+
+        if (data) {
+            setProject(data)
         } else {
-            console.log(data)
+            navigate('/')
+            return
         }
     }, [id, navigate])
-    return <h1>Projet</h1>
+
+    if (!project) {
+        return (
+            <main>
+                <h1>Chargement...</h1>
+            </main>
+        )
+    }
+
+    return (
+        <main>
+            <h1>{project.name}</h1>
+        </main>
+    )
 }
 
 export default Projet
