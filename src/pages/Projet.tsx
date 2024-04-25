7ef5a9b6ddd9fa9d27c94a4d6bc633ee07a841f6
BetaPeche <lolopellegrino@gmail.com>
2024-04-25 10:36:46 +0200

    Add data

--- src/pages/Projet.tsx
+++ src/pages/Projet.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import dataProjects from '../assets/data/dataProjects.json'
+
 const Projet = () => {
+    const { id } = useParams()
+    console.log(id)
+    const navigate = useNavigate()
+    // const [hosting, setHosting] = useState()
+
+    useEffect(() => {
+        const data = dataProjects.find((array) => array.name == id)
+        if (!data) {
+            navigate('/')
+        } else {
+            console.log(data)
+        }
+    }, [id, navigate])
     return <h1>Projet</h1>
 }
 
