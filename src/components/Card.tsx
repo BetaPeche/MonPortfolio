e7b3685976e105d8b5082e9ac188e3814e1fa43d
BetaPeche <lolopellegrino@gmail.com>
2024-04-25 09:54:46 +0200

    Change path of link

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ data }) => {
     return (
-        <Link to={`/${data.name.replace(' ', '-')}`} className="card">
+        <Link to={`projet/${data.name.replace(' ', '-')}`} className="card">
             <h3>{data.name}</h3>
             <p>{data.keywords}</p>
             <img src={data.image} alt={`Image du site ${data.name}`} />
