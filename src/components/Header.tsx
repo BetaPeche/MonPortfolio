9dd652a3dac63865cc9577136fcf11d77fedaea7
BetaPeche <lolopellegrino@gmail.com>
2024-04-26 08:46:10 +0200

    Use 'useLocation' for conditional display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import useThemeStore from '../themeStore'
 
 const Header = () => {
     const { theme, toggleTheme } = useThemeStore()
+    const sampleLocation = useLocation()
 
     return (
         <header>
             <Link to="/">Loïc Pellegrino</Link>
             <nav>
-                <ul>
-                    <li>
-                        <a href="#projects">Projets</a>
-                    </li>
-                    <li>
-                        <a href="#about-me">À propos</a>
-                    </li>
-                </ul>
+                {sampleLocation.pathname === '/' && (
+                    <ul>
+                        <li>
+                            <a href="#projects">Projets</a>
+                        </li>
+                        <li>
+                            <a href="#about-me">À propos</a>
+                        </li>
+                    </ul>
+                )}
                 <button
                     className={theme === 'dark' ? 'dark' : 'light'}
                     onClick={toggleTheme}
