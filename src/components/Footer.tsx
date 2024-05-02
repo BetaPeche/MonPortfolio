cc8eef88fe91679c96512f4bc9da5d6710cee3de
BetaPeche <lolopellegrino@gmail.com>
2024-05-02 15:21:14 +0200

    Replace Link by anchor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,24 @@
-import { Link } from 'react-router-dom'
-
 const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
                 <p>Made with ❤️ by me</p>
-                <p>Contact: test@test.com</p>
+                <a href="mailto:lolopellegrino@gmail.com">Me contacter</a>
                 <div className="footer-links">
-                    <Link
-                        to="https://github.com/BetaPeche"
+                    <a
+                        href="https://github.com/BetaPeche"
                         target="_blank"
                         rel="noreferrer"
                     >
                         <img src="/img/githubdark.webp" alt="logo github" />
-                    </Link>
-                    <Link
-                        to="https://www.linkedin.com/in/lo%C3%AFc-pellegrino-927295295/"
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/lo%C3%AFc-pellegrino-927295295/"
                         target="_blank"
                         rel="noreferrer"
                     >
                         <img src="/img/linkedin.webp" alt="logo linkedin" />
-                    </Link>
+                    </a>
                 </div>
             </div>
         </footer>
